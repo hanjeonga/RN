c3e149530dce79490ef7b28c2c919ba0abe6ea89
Jung-A Han <mongndam@naver.com>
2025-04-23 16:27:55 +0900

    [Feat] Button, Badge Atom Components

--- component/App.js
+++ component/App.js
@@ -2,6 +2,9 @@ import { StyleSheet, View } from "react-native";
 import LocalImage from "./src/components/LocalImage";
 import RemoteImage from "./src/components/RemoteImage";
 import Icons from "./src/components/Icons";
+import Badge from "./src/components/Badge";
+import { Typography } from "./src/components/Typography";
+import Button from "./src/components/Button";
 
 export default function App() {
   return (
@@ -9,11 +12,13 @@ export default function App() {
       <Typography color="red" fontSize={20}>
         이것은 텍스트 입니다.
       </Typography>
+
       <LocalImage
         localAsset={require("./assets/favicon.png")}
         width={50}
         height={50}
       />
+
       <RemoteImage
         url={
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9aeHFWXHfDhJ_xzPneJlEsKVdu-tZ5vGoow&s"
@@ -21,7 +26,25 @@ export default function App() {
         width={100}
         height={200}
       />
+
       <Icons name="home" size={40} color="red" />
+
+      <View>
+        <Badge fontSize={10}>
+          <Typography>BADGE</Typography>
+        </Badge>
+      </View>
+      <View>
+        <Badge>
+          <Icons name="home" size={50} color={black} />
+        </Badge>
+      </View>
+
+      <View style={{ flexDirection: "row", marginTop: 32 }}>
+        <Button onPress={() => console.log("PRESSED BUTTON")}>
+          <Typography>TEXT BUTTON</Typography>
+        </Button>
+      </View>
     </View>
   );
 }
